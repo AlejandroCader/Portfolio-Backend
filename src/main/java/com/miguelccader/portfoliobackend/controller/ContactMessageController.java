cd6b0bf52917862c23a2d829d8c21905c2e2d755
AlejandroCader <00019018@uca.edu.sv>
2025-03-31 20:48:33 +0200

    Adding GetMapping functionality

--- src/main/java/com/miguelccader/portfoliobackend/controller/ContactMessageController.java
+++ src/main/java/com/miguelccader/portfoliobackend/controller/ContactMessageController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/contact")
 @RequiredArgsConstructor
@@ -17,5 +19,11 @@ public class ContactMessageController {
     public ResponseEntity<ContactMessage> receiveMessage(@RequestBody ContactMessage message){
         ContactMessage savedMessage = contactMessageService.saveMessage(message);
         return ResponseEntity.ok(savedMessage);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ContactMessage>> getAllContacts(){
+        List<ContactMessage> messages = contactMessageService.getAllMessages();
+        return ResponseEntity.ok(messages);
     }
 }
--- src/main/java/com/miguelccader/portfoliobackend/service/ContactMessageService.java
+++ src/main/java/com/miguelccader/portfoliobackend/service/ContactMessageService.java
@@ -2,14 +2,22 @@ package com.miguelccader.portfoliobackend.service;
 
 import com.miguelccader.portfoliobackend.model.ContactMessage;
 import com.miguelccader.portfoliobackend.repository.ContactMessageRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-@RequiredArgsConstructor
 public class ContactMessageService {
     private final ContactMessageRepository repository;
     public ContactMessage saveMessage(ContactMessage message){
         return repository.save(message);
+    }
+
+    public ContactMessageService (ContactMessageRepository repository){
+        this.repository = repository;
+    }
+
+    public List<ContactMessage> getAllMessages(){
+        return repository.findAll();
     }
 }
